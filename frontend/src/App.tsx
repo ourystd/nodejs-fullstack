191bc686b95b3815b195a4b7f097e247d2a779e6
Oury Diallo <amadoury313@gmail.com>
2024-03-24 19:29:36 +0000

    tailwind added to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,50 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function incrementX() {
+    setCount(count + 1);
+    setCount(count + 1);
+  }
+
+  function incrementY() {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  }
 
   return (
+    <div>
+      <div>{incrementX()!}</div>
+      <div>{incrementY()!}</div>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="bg-slate-200 h-screen w-svw">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
 export default App
