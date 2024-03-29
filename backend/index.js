e7b5a0a942996b0a6ccfc2aac803dbf9fe7b811a
Oury Diallo <amadoury313@gmail.com>
2024-03-29 20:39:54 +0000

    security update + enhacement
    - Express upgraded to latest for security reasons
    - morganjs added for logging

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,40 @@
 require("./database").connect();
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 const cors = require("cors");
 const userRouter = require("./routes/users");
 const auth = require("./middleware/auth");
+
+// Define custom format for logging
+morgan.token("requestLog", (req, res) => {
+  const forwardedFor = req.headers["x-forwarded-for"];
+  const ip = forwardedFor
+    ? forwardedFor.split(",")[0]
+    : req.socket.remoteAddress;
+
+  if (req.method !== "OPTIONS") {
+    return JSON.stringify(
+      {
+        method: req.method,
+        url: req.url,
+        originalUrl: req.originalUrl,
+        status: res.statusCode,
+        userAgent: req.headers["user-agent"],
+        timestamp: new Date().toISOString(),
+        ip,
+      },
+      null,
+      2
+    );
+  }
+
+  return `OPTIONS ${req.url} ${res.statusCode} ${ip}`;
+});
+
+// Use morgan middleware with custom format
+app.use(morgan(":requestLog"));
 
 app.use(cors());
 app.use(express.json());
