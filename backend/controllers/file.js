7bf5e0d07f6610595f7dedd547a81fb765f516da
Oury Diallo <amadoury313@gmail.com>
2024-04-03 10:57:20 +0000

    feat: teext processing

--- backend/controllers/file.js
+++ backend/controllers/file.js
@@ -1,11 +1,59 @@
 const { FileModel, validateFile, serializeFile } = require("../models/file");
+const fs = require("fs");
+const readline = require("readline");
+const SpellChecker = require("simple-spellchecker").getDictionarySync("en-GB");
+const stringSimilarity = require("string-similarity");
+
+const spellCheck = async (path) => {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(path),
+    output: process.stdout,
+    console: false,
+  });
+
+  let text = "";
+
+  for await (const line of readInterface) {
+    const correctedLine = line
+      .split(" ")
+      .map((word) => {
+        if (word && !SpellChecker.spellCheck(word)) {
+          const suggestions = SpellChecker.getSuggestions(word);
+
+          console.log({ word, suggestions });
+
+          const matches = stringSimilarity.findBestMatch(
+            word,
+            suggestions.length > 0 ? suggestions : [word]
+          );
+
+          return matches.bestMatch.target;
+        } else return word;
+      })
+      .join(" ");
+
+    text += correctedLine + "\n";
+  }
+
+  fs.writeFile(path, text, (err, res) => {
+    if (err) console.log("error", err);
+  });
+};
 
 exports.upload = async (req, res) => {
   try {
     const { error } = validateFile(req.body);
     if (error) {
       console.log({ error });
-      return res.status(400).send(error.details[0].message);
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    if (req.file.mimetype === "text/plain") {
+      spellCheck(req.file.path);
     }
 
     const { name, description } = req.body;
